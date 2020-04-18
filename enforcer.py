59234bdfeaa6dc0c7caf757f2edfad61f3d64fdb
avn-0216-m <maidenofthebirdeternal@gmail.com>
2020-04-18 19:54:58 +0100

    Can successfully connect to SQL database

--- enforcer.py
+++ enforcer.py
@@ -1,19 +1,31 @@
-#import core
-import discord
+#import core modules
 import sys
+import discord
+from discord.ext import commands
 import json
-#import modules
-#import utility
-#import data structures
+import mysql.connector
+#import enforcer modules
+#import utility modules 
+#import data structure modules
 
-bot = discord.ext.commands.Bot(command_prefix='//', case_insensitive=True)
-print("Getting SQL details")
-with open("sql_details.json") as sql_file:
-    sql_details = json.load(sql_file)
-    for detail in details:
-        print(detail)
-        
-bot.run(sys.argv[1])
+print("Getting SQL and token details")
+with open("secret_details.json") as secret_file:
+    secret_details = json.load(secret_file)
+    for key in secret_details:
+        print("--------------")
+        print(key)
+        print(secret_details[key])
+    db_host = secret_details['db_host']
+    db_user = secret_details['db_user']
+    db_pass = secret_details['db_pass']
+    bot_token = secret_details['bot_token']
+print("Establishing connection with database.")
+database = mysql.connector.connect(host=db_host, user=db_user, passwd=db_pass)
+print(database)
+print("Connection established.")
+print("Starting bot.")
+bot = commands.Bot(command_prefix="//")
+bot.run(bot_token)
 
 @bot.command()
 async def reset(context):
