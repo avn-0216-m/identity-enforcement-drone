d7036804ca540453ee4d49bb07b3155abb230a9b
avn-0216-m <maidenofthebirdeternal@gmail.com>
2020-04-18 20:05:44 +0100

    Added command to reset database

---
+++ database_constants.py
@@ -0,0 +1,2 @@
+DATABASE_NAME = 'enforcement_drone'
+TEST_TABLE_NAME = 'test_table'
--- enforcer.py
+++ enforcer.py
@@ -7,7 +7,31 @@ import mysql.connector
 #import enforcer modules
 #import utility modules 
 #import data structure modules
+from database_constants import DATABASE_NAME, TEST_TABLE_NAME
 
+bot = commands.Bot(command_prefix="")
+
+@bot.command()
+async def db_reset(context):
+    print("Resetting database.")
+    cursor.execute("DROP DATABASE IF EXISTS " + DATABASE_NAME)
+    print("Database dropped.")
+    cursor.execute("CREATE DATABASE " + DATABASE_NAME)
+    print("Database created.")
+    cursor.execute("USE " + DATABASE_NAME)
+    cursor.execute("CREATE TABLE " + TEST_TABLE_NAME + "(message_id int not null primary key auto_increment, message varchar(255))")
+    print("Test table created.")
+    await context.send("Database reset.")
+
+@bot.command()
+async def db_push(context, argument):
+    print("Pushing argument to database.")
+
+@bot.command()
+async def db_list(context, argument):
+    print("Listing entries in database")
+
+#Main setup begins here.
 print("Getting SQL and token details")
 with open("secret_details.json") as secret_file:
     secret_details = json.load(secret_file)
@@ -23,18 +47,6 @@ print("Establishing connection with database.")
 database = mysql.connector.connect(host=db_host, user=db_user, passwd=db_pass)
 print(database)
 print("Connection established.")
+cursor = database.cursor()
 print("Starting bot.")
-bot = commands.Bot(command_prefix="//")
 bot.run(bot_token)
-
-@bot.command()
-async def reset(context):
-    print("Resetting database.")
-
-@bot.command()
-async def push(context, argument):
-    print("Pushing argument to database.")
-
-@bot.command()
-async def list(context, argument):
-    print("Listing entries in database")
