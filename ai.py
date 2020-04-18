1b1349b831068aa6cff84df27332122173f00997
avn-0216-m <maidenofthebirdeternal@gmail.com>
2020-04-18 19:13:48 +0100

    Added gitignore and some test commands

--- ai.py
+++ ai.py
@@ -8,3 +8,15 @@ import sys
 bot = discord.ext.commands.Bot(command_prefix='//', case_insensitive=True)
 print("Starting bot!")
 bot.run(sys.argv[1])
+
+@bot.command()
+async def reset(context):
+    print("Resetting database.")
+
+@bot.command()
+async def push(context, argument):
+    print("Pushing argument to database.")
+
+@bot.command()
+async def list(context, argument):
+    print("Listing entries in database")
