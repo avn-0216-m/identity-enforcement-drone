c300e2de9c9749058757d3be51d93d9780f1c48d
avn-0216-m <maidenofthebirdeternal@gmail.com>
2020-08-08 23:11:16 +0100

    fixed the attribute set db function

--- text.py
+++ text.py
@@ -1,4 +1,4 @@
-new_enforcement = ["Bye bye old identity!", "ERROR: Old identity deleted... Just kidding!" "This new identity suits you!", "Enjoy being a {}!", "Oh yeah, you're gonna *love* this one.", "Have fun, cutie!", "You weren't planning on keeping that, right?", "Surprise!"]
+new_enforcement = ["Bye bye old identity!", "ERROR: Old identity deleted... Just kidding!", "This new identity suits you!", "Enjoy being a {}!", "Oh yeah, you're gonna *love* this one.", "Have fun, cutie!", "You weren't planning on keeping that, right?", "Surprise!"]
 
 not_domming = ["I'm not saying they wouldn't make a good {}, but...", "Consent is required.", "Consent is not optional (unless they're into that).", "Please don't be pushy.", "You are not dominating that user.", "Ask politely.", "Sorry, my hands are tied (not in the hot way).", "You are not in the sudoers file. This incident will be reported.", "Hey, dinner and a movie first, buddy."]
 
